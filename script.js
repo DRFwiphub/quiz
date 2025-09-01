25162e31e8b065a6aeb08a6fe3fb2b7008d4896e
drfwiphub <danielr.flatt@gmail.com>
2025-09-01 20:14:31 +0100

    functionality updated, randomised questions, added flavour text

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const startBtn = document.getElementById("start-btn");
 
 startBtn.addEventListener('click', () => {
-    startBtn.style.display = "none"; // Hide start button
-    document.querySelector('.quiz').style.display = "block"; // Show quiz
+    startBtn.style.display = "none"; 
+    document.querySelector('.quiz').style.display = "block"; 
     startQuiz();
 });
 
@@ -11,10 +11,12 @@ const questions = [
         question: "Who is the Dragon Reborn?",
         answers: [
             { text: "Mat", correct: false },
-            { text: "Perrin", correct: false },
             { text: "Rand", correct: true },
+            { text: "Perrin", correct: false },
             { text: "Thom", correct: false }    
-        ]
+        ],
+        right: "Rand is the Dragon Reborn",
+        wrong: "Rand Al'Thor is the Dragon Reborn"
     },
     {
         question: "Who doesn't become royalty?",
@@ -23,17 +25,31 @@ const questions = [
             { text: "Rand", correct: false },
             { text: "Lan", correct: true },
             { text: "Perrin", correct: false }    
-        ]
+        ],
+        right: "Lan already is Royalty",
+        wrong: "Lan was born a Prince in the Borderlands"
     },
-
+    {
+        question: "What is Mat's weapon of choice?",
+        answers: [
+            { text: "Spear", correct: false },
+            { text: "Bow", correct: false },
+            { text: "Knives", correct: false },
+            { text: "Quarterstaff", correct: true }    
+        ],
+        right: "Mat's weapon of choice is a quarterstaff",
+        wrong: "While proficient in all four weapons, he prefers a quarterstaff"
+    },
     {
         question: "who is Rand'd half brother?",
         answers: [
+            { text: "Galad", correct: true },
             { text: "Moraine", correct: false },
             { text: "Julian", correct: false },
-            { text: "Galad", correct: true },
             { text: "Logain", correct: false }    
-        ]
+        ],
+        right: "Galad is Rand's brother",
+        wrong: "Rand's mother was Tigraine, the former Queen of Andor and Galad's Birthmother"
     },
     {
         question: "Who is not among the Chosen?",
@@ -42,7 +58,29 @@ const questions = [
             { text: "Damandred", correct: false },
             { text: "Tam", correct: true },
             { text: "Semirhage", correct: false }    
-        ]
+        ],
+        right: "Tam raised Rand in the Two Rivers",
+        wrong: "Lanfear, Demandred and Semirhage are all part of the Chosen"
+    },
+    {
+        question: "is ruby cute",
+        answers: [
+            { text: "false", correct: false},
+            { text: "true", correct: true}
+        ],
+        right: "Ruby is cute!",
+        wrong: "You should know Ruby is cute!"
+    },
+    {
+        question: "What's Ruby's oddest food choice?",
+        answers: [
+            { text: "Pandabao!!", correct: false },
+            { text: "Onogiryyy!", correct: false },
+            { text: "Pastrybun bun bun bun", correct: false },
+            { text: "Anythinging tastyyyy", correct: false },    
+            { text: "RaNdOm..", correct: right }
+        ],
+        wrong: "SShhhh! It's a secret!"
     },
 ];
 
@@ -58,6 +96,7 @@ function startQuiz(){
     currentQuestionIndex = 0;
     score = 0;
     nextButton.innerHTML = "next";
+    shuffle(questions); // Shuffle once at the start!
     showQuestion();
 }
 
@@ -66,10 +105,11 @@ function showQuestion(){
     
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
 
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
     currentQuestion.answers.forEach(answer => {
         const button = document.createElement('button');
+
         button.innerHTML = answer.text;
         button.classList.add('btn');
         answerButton.appendChild(button);
@@ -77,7 +117,26 @@ function showQuestion(){
             button.dataset.correct = answer.correct;
         }
         button.addEventListener("click", selectAnswer);
-    });       
+    });   
+}
+function flavourRight() {
+    let current = questions[currentQuestionIndex];
+    const flavourText = document.getElementById("flavour-text");
+    flavourText.innerHTML = `Correct, ${current.right}`;
+    var rht = document.getElementsByClassName('flavour');
+    if (rht.length > 0) {
+        rht[0].style.display = "block";
+    }
+}
+
+function flavourWrong() {
+    let current = questions[currentQuestionIndex];
+    const flavourText = document.getElementById("flavour-text");
+    flavourText.innerHTML = `${current.wrong}`;
+    var wng = document.getElementsByClassName('flavour');
+    if (wng.length > 0) {
+        wng[0].style.display = "block";
+    }
 }
 
 function resetState(){
@@ -86,38 +145,41 @@ function resetState(){
         answerButton.removeChild(answerButton.firstChild);
   }
 }
-
 function selectAnswer(e){
     const selectBtn = e.target;
     const isCorrect = selectBtn.dataset.correct === "true";
     if(isCorrect){
         selectBtn.classList.add("correct");
         score++;
+        flavourRight();
     }else{
         selectBtn.classList.add("incorrect");
+        flavourWrong();
     }
     Array.from(answerButton.children).forEach(button => {
         if(button.dataset.correct === "true"){
-            button.classList.add("correct");
+            button.classList.add("correct");            
         }
         button.disabled = true;
     });
     nextButton.style.display = "block";
 }
-
 function showScore(){
     resetState();
     questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
-    replayButton.style.display = "block"; // Show button
-};
-
+    replayButton.style.display = "block"; 
+}
 replayButton.addEventListener('click', () => {
-    replayButton.style.display = "none"; // Hide start button
-    document.querySelector('.quiz').style.display = "block"; // Show quiz
+    replayButton.style.display = "none"; 
+    document.querySelector('.quiz').style.display = "block"; 
     startQuiz();
 });
-
 function handleNextButton(){
+    var flavourDivs = document.getElementsByClassName('flavour');
+    if (flavourDivs.length > 0) {
+        flavourDivs[0].style.display = "none";
+    }
+
     currentQuestionIndex++
     if(currentQuestionIndex < questions.length){
         showQuestion();
@@ -131,5 +193,12 @@ nextButton.addEventListener("click", () =>{
     }else{
         startQuiz();
     }
+    
 });
-//startQuiz();
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
